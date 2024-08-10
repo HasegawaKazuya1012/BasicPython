f27487cdb5c04ef77e43d645bc9e4cacae02923f
your@email.com <yakiimo041012@gmail.com>
2024-08-10 20:37:27 +0900

    問一完成

--- pi.py
+++ pi.py
@@ -3,5 +3,14 @@ text = """
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
-# TODO
+#type(text) = str
 
+# TODO
+#text.splitdeで半角スペースで英文を区切り,.をstrip(',.')でカウントしないようにする
+words = [word.strip(',.') for word in text.split()]
+#map関数でlen関数をwordsに適用する
+lengths = list(map(len , words))
+#join関数を用いて文字数を連結させる
+answer = """""".join(map(str, lengths))
+
+print(answer)
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,25 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = 61
+b = 10
 
 # TODO
+if a <= 1:
+    print("aは素数ではありません")
+else:
+    for i in range(2 , int(a**0.5) + 1):
+        if a % i == 0:
+            print("aは素数ではありません")
+            break
+        else:
+            print("aは素数です")
+            break
+
+if b <= 1:
+    print("bは素数ではありません")
+else:
+    for i in range(2 , int(b**0.5)+1):
+        if b % i == 0:
+            print("bは素数ではありません")
+            break
+        else:
+                print("bは素数です")
+                break
