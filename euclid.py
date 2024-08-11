a004cdf0639daab5d58b63e749c9411fa2c802c8
your@email.com <yakiimo041012@gmail.com>
2024-08-11 12:23:59 +0900

    問三完成

--- euclid.py
+++ euclid.py
@@ -1,4 +1,24 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
-
 # TODO
+#import math
+#xとyをきめる
+#result = math.gcd(x, y)
+#これでも可能
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b #aをbに、bをa % bにループさせていく
+    return a
+#whileのループが終わった後に戻り値を出す
+a = 10
+b = 20
+result = gcd(a, b)
+print(f"{a}と{b}の最小公倍数は{result}です")   
+
+a = 14
+b = 91
+result = gcd(a, b)
+print(f"{a}と{b}の最小公倍数は{result}です")
+
+a = 91
+b = 14
+result = gcd(a, b)
+print(f"{a}と{b}の最小公倍数は{result}です")
